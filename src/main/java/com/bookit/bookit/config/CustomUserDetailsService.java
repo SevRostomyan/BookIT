e68230ad68e530427992645b8913c3f1a72d85e6
sevrostomyan <sev-rostomyan@hotmail.com>
2023-10-14 20:28:21 +0200

    småändringar

---
+++ src/main/java/com/bookit/bookit/config/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.bookit.bookit.config;
+
+import com.bookit.bookit.entity.user.User;
+import com.bookit.bookit.repository.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
--- src/main/java/com/bookit/bookit/config/SecurityConfig.java
+++ src/main/java/com/bookit/bookit/config/SecurityConfig.java
@@ -2,16 +2,22 @@ package com.bookit.bookit.config;
 
 import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
+import com.bookit.bookit.config.ApplicationConfig.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import com.bookit.bookit.config.JwtAuthenticationFilter;
 
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -20,30 +26,50 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final Filter jwtAuthFilter;
+    //private final Filter jwtAuthFilter; //default filter
+    private final JwtAuthenticationFilter jwtAuthFilter; //Custom filter
     private final AuthenticationProvider authenticationProvider;
-    private JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private JwtService jwtService; //Kolla varför det står här och inte används
+    private CustomUserDetailsService customUserDetailsService;
 
+    @Autowired
+    public SecurityConfig(
+            JwtAuthenticationFilter jwtAuthFilter,
+            AuthenticationProvider authenticationProvider,
+            CustomUserDetailsService customUserDetailsService,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.authenticationProvider = authenticationProvider;
+        this.customUserDetailsService = customUserDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder);  // Use the injected PasswordEncoder
+    }
     @Bean
     public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
-                        authorize -> authorize.requestMatchers(new AntPathRequestMatcher("/api/auth/**"))   //We are whitelistning a list of application strings/paterns that will be allowed bellow.) All endpoints will be authenticated in /api/auth/
+                        authorize -> authorize.requestMatchers(new AntPathRequestMatcher("/api/auth/**"))
                                 .permitAll()
-                                .anyRequest() //Allt annat utöver listan ska krävas autentisering
+                                .anyRequest()
                                 .authenticated()
                 )
-                .sessionManagement((sessionManagement) ->
+                .sessionManagement(sessionManagement ->
                         sessionManagement
                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .logout(logout -> logout.logoutUrl("/api/auth/logout")  //För utloggning anropa url-n till vänster
+                .logout(logout -> logout.logoutUrl("/api/auth/logout")
                         .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
                 )
                 .build();
-
     }
+
+
 }
